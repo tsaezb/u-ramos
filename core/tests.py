148d623bba882e85c7ca245071f52d088c8f59f3
Tomas Saez <tomas.saezbin@gmail.com>
2017-06-16 21:57:25 +0000

    add test & travis

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+class U_ramos_test(TestCase):
+    
+    def test_dumy(self):
+        self.assertEquals(0,0)
+        
